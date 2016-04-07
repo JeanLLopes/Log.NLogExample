d07a48c6663c343e242e146e1a7de54c185efbdc
JeanLLopes <jean.llopes@outlook.com>
2016-04-07 00:22:07 -0300

    Add Project Initial

---
+++ Log.NLogExample.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Log.NLogExample.Console
+{
+    class Program
+    {
+
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            int k = 42;
+            int l = 100;
+
+            logger.Trace("Sample trace message, k={0}, l={1}", k, l);
+            logger.Debug("Sample debug message, k={0}, l={1}", k, l);
+            logger.Info("Sample informational message, k={0}, l={1}", k, l);
+            logger.Warn("Sample warning message, k={0}, l={1}", k, l);
+            logger.Error("Sample error message, k={0}, l={1}", k, l);
+            logger.Fatal("Sample fatal error message, k={0}, l={1}", k, l);
+            logger.Log(LogLevel.Info, "Sample informational message, k={0}, l={1}", k, l);
+        }
+
+
+        
+        
+    }
+}
