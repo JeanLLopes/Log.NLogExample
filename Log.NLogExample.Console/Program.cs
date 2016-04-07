a1565ffb16673cb5092cecc5a89b47126ffe1048
JeanLLopes <jean.llopes@outlook.com>
2016-04-07 00:51:44 -0300

    Alter Project To SendMail Alter Project To CreateText File

--- Log.NLogExample.Console/Program.cs
+++ Log.NLogExample.Console/Program.cs
@@ -13,16 +13,30 @@ namespace Log.NLogExample.Console
         private static Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            int k = 42;
-            int l = 100;
+            CalculoTeste();
+        }
 
-            logger.Trace("Sample trace message, k={0}, l={1}", k, l);
-            logger.Debug("Sample debug message, k={0}, l={1}", k, l);
-            logger.Info("Sample informational message, k={0}, l={1}", k, l);
-            logger.Warn("Sample warning message, k={0}, l={1}", k, l);
-            logger.Error("Sample error message, k={0}, l={1}", k, l);
-            logger.Fatal("Sample fatal error message, k={0}, l={1}", k, l);
-            logger.Log(LogLevel.Info, "Sample informational message, k={0}, l={1}", k, l);
+        private static void CalculoTeste()
+        {
+            int zero = 0;
+            try
+            {
+                var result = 100 / zero;
+            }
+            catch (Exception ex)
+            {
+                logger.ErrorException("Error", ex);
+                //int k = 42;
+                //int l = 100;
+                //logger.Trace("Sample trace message, k={0}, l={1}", k, l);
+                //logger.Debug("Sample debug message, k={0}, l={1}", k, l);
+                //logger.Info("Sample informational message, k={0}, l={1}", k, l);
+                //logger.Warn("Sample warning message, k={0}, l={1}", k, l);
+                //logger.Error("Sample error message, k={0}, l={1}", k, l);
+                //logger.Fatal("Sample fatal error message, k={0}, l={1}", k, l);
+                //logger.Log(LogLevel.Info, "Sample informational message, k={0}, l={1}", k, l);
+
+            }
         }
 
 
